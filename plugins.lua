95873e9f6ab6012fae438407c51785237d82dc7d
Lucas Guerra <lucasguerra34@gmail.com>
2023-12-22 00:28:03 -0300

    nvchad config

---
+++ plugins.lua
@@ -0,0 +1,66 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      require("core.utils").load_mappings("dap")
+      require('dap.ext.vscode').load_launchjs('.vscode/launch.json',{})
+    end
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+  {
+      "github/copilot.vim",
+      lazy = false,
+      config = function()  -- Mapping tab is already used by NvChad       
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";  -- The mapping is set to other key, see custom/lua/mappings  
+      -- or run <leader>ch to see copilot mapping section  
+      end
+  },
+  {
+    "f-person/git-blame.nvim",
+    lazy = false
+  },
+}
+return plugins
