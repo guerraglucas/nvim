8daa70c976ac330b57ef24f28f08f353c2a03d88
guerraglucas <lucasguerra34@gmail.com>
2025-07-25 18:14:37 -0300

    copiando um nvim do youtube rs

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -31,7 +31,6 @@ return {
                 "golangci-lint",
                 "eslint_d",
                 "luacheck", -- Lua linting
-                "pint",     -- Laravel Pint for PHP (formatting & linting)
 
                 -- Additional useful tools
                 "delve",      -- Go debugger
